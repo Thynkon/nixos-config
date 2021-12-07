bff1c4e0ec92e659f0f5b78a815340944f9b4d8b
thynkon <thynkon@protonmail.com>
2021-12-07 19:33:05 +0100

    Install firmware updater and optimize power usage on laptops (enable turbo boost)

--- configuration.nix
+++ configuration.nix
@@ -5,73 +5,82 @@
 { config, pkgs, lib, ... }:
 
 {
-    imports = [ # Include the results of the hardware scan.
-        ./hardware-configuration.nix
-        ./packages.nix
-        ./unfree-packages.nix
-        ./users/thynkon
-#        ./machines/<machine-type>
-        ./machines/laptop-lenovo
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./packages.nix
+    ./unfree-packages.nix
+    ./users/thynkon
+    #        ./machines/<machine-type>
+    ./machines/laptop-lenovo
+  ];
 
-    boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
+  # update firmware
+  services.fwupd.enable = true;
+  hardware.mwProCapture.enable = true;
 
-    networking.hostName = "nixos"; # Define your hostname.
+  boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
 
-    # Set your time zone.
-    time.timeZone = "Europe/Zurich";
+  networking.hostName = "nixos"; # Define your hostname.
 
-    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-    # Per-interface useDHCP will be mandatory in the future, so this generated config
-    # replicates the default behaviour.
-    networking.useDHCP = false;
-    networking.networkmanager.enable = true;
+  # Set your time zone.
+  time.timeZone = "Europe/Zurich";
 
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
-    console.useXkbConfig = true;
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.networkmanager.enable = true;
 
-    # Enable CUPS to print documents.
-    # services.printing.enable = true;
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.useXkbConfig = true;
 
-    # Enable sound.
-    sound.enable = true;
-    hardware.pulseaudio.enable = true;
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    services.xserver.libinput.enable = true;
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
-    # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
 
-    fonts.fonts = with pkgs; [
-            (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "FantasqueSansMono" ]; })
-    ];
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
-    # SHELL
-    users.defaultUserShell = pkgs.zsh;
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "FantasqueSansMono" ]; })
+  ];
 
-    hardware.bluetooth.enable = true;
-    services.blueman.enable = true;
+  # SHELL
+  users.defaultUserShell = pkgs.zsh;
 
-    nix.gc = {
-            automatic = true;
-            dates = "weekly";
-            options = "--delete-older-than 14d";
-    };
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  nix = {
+    # Enable flakes support
+    package = pkgs.nixFlakes;
     # Free up to 1GiB whenever there is less than 100MiB left
-    nix.extraOptions = ''
-            min-free = ${toString (100 * 1024 * 1024)}
-            max-free = ${toString (1024 * 1024 * 1024)}
+    extraOptions = ''
+      min-free = ${toString (100 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
+      experimental-features = nix-command flakes
     '';
+  };
 
-
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "21.11"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
 }
--- users/thynkon/battery/default.nix
+++ users/thynkon/battery/default.nix
@@ -1,33 +1,65 @@
 { config, pkgs, ... }:
 
 {
-	# Define a user account. Don't forget to set a password with ‘passwd’.
-	users.users.thynkon = {
-		packages = with pkgs; [
-			# battery
-			powertop
-			tlp
-			#tlp-rdw
-		];
-	};
+  users.users.thynkon = {
+    packages = with pkgs; [
+      powertop
+      tlp
+    ];
+  };
 
-        # Reference: https://discourse.nixos.org/t/thinkpad-t470s-power-management/8141/4
-        services.tlp = {
-                enable = true;
-                settings = {
-                        "CPU_SCALING_GOVERNOR_ON_AC" = "performance";
-                        "CPU_SCALING_GOVERNOR_ON_BAT" = "powersave";
-                        # Runtime Power Management for PCI(e) bus devices: on=disable, auto=enable.
-                        # Default: on (AC), auto (BAT)
-                        "RUNTIME_PM_ON_AC" = "on";
-                        "RUNTIME_PM_ON_BAT" = "auto";
+  # Reference: https://discourse.nixos.org/t/thinkpad-t470s-power-management/8141/4
+  boot.initrd.availableKernelModules = [
+    # trimmed irrelevant ones
+    "thinkpad_acpi"
+  ];
 
-                };
-        };
+  powerManagement.cpuFreqGovernor = "ondemand";
 
-        boot.initrd.availableKernelModules = [
-                # trimmed irrelevant ones
-                "thinkpad_acpi"
-        ];
-        services.upower.enable = true;
+  services = {
+    upower.enable = true;
+#    auto-cpufreq.enable = true;
+    tlp = {
+      enable = true;
+      # Reference: https://linrunner.de/tlp/settings/processor.html
+      extraConfig = ''
+        START_CHARGE_THRESH_BAT0=75
+        STOP_CHARGE_THRESH_BAT0=80
+
+        CPU_ENERGY_PERF_POLICY_ON_AC=performance
+        CPU_ENERGY_PERF_POLICY_ON_BAT=balance_power
+
+        CPU_SCALING_GOVERNOR_ON_AC=schedutil
+        CPU_SCALING_GOVERNOR_ON_BAT=schedutil
+
+        # Refer to the output of tlp-stat -p to determine the active scaling driver and available governors.
+        # https://linrunner.de/tlp/settings/processor.html#cpu-scaling-min-max-freq-on-ac-bat
+        CPU_SCALING_MIN_FREQ_ON_AC=800000
+        CPU_SCALING_MAX_FREQ_ON_AC=3500000
+        CPU_SCALING_MIN_FREQ_ON_BAT=800000
+        CPU_SCALING_MAX_FREQ_ON_BAT=2300000
+
+        # Enable audio power saving for Intel HDA, AC97 devices (timeout in secs).
+        # A value of 0 disables, >=1 enables power saving (recommended: 1).
+        # Default: 0 (AC), 1 (BAT)
+        SOUND_POWER_SAVE_ON_AC=0
+        SOUND_POWER_SAVE_ON_BAT=1
+
+        # Runtime Power Management for PCI(e) bus devices: on=disable, auto=enable.
+        # Default: on (AC), auto (BAT)
+        RUNTIME_PM_ON_AC=on
+        RUNTIME_PM_ON_BAT=auto
+
+        # Enable intel turbo boost on AC
+        CPU_BOOST_ON_AC=1
+        CPU_BOOST_ON_BAT=0
+
+        # Battery feature drivers: 0=disable, 1=enable
+        # Default: 1 (all)
+        NATACPI_ENABLE=1
+        TPACPI_ENABLE=1
+        TPSMAPI_ENABLE=1
+      '';
+    };
+  };
 }
