ea3742ae0a87fdf42e438ba974df1076844b1ed2
thynkon <thynkon@protonmail.com>
2022-02-04 20:36:47 +0100

    Fix fprintd auth problem

--- machines/laptop-lenovo/security.nix
+++ machines/laptop-lenovo/security.nix
@@ -5,4 +5,12 @@
         services.fwupd.enable = true;
         security.pam.services.login.fprintAuth = true;
         security.pam.services.xscreensaver.fprintAuth = true;
+
+        security.polkit.extraConfig = ''
+            polkit.addRule(function (action, subject) {
+              if (action.id == "net.reactivated.fprint.device.enroll") {
+                return subject.user == "root" ? polkit.Result.YES : polkit.result.NO
+              }
+            })
+          '';
 }
