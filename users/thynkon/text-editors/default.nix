a00fc96e94582300643c9c4c0102afe385047910
thynkon <thynkon@protonmail.com>
2022-05-30 22:36:09 +0200

    Install neovim and LSP servers using home-manager

--- users/thynkon/text-editors/default.nix
+++ users/thynkon/text-editors/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+  unstable = import <nixos-unstable> { };
 in
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -9,26 +9,9 @@ in
     packages = with pkgs; [
       ## text editors
       #nodejs-intelephense
-      #sql-language-server
-      cmake-language-server
-      gopls # go lsp server
-      # jetbrains.idea-ultimate
-      jetbrains.phpstorm
-      nodePackages.bash-language-server
-      nodePackages.vscode-css-languageserver-bin
-      nodePackages.vscode-html-languageserver-bin
-      nodePackages.vscode-json-languageserver
-      python-language-server
-      python310Packages.pynvim
-      rust-analyzer
-      sumneko-lua-language-server
-      texlab # latex lsp server
-      tree-sitter # syntax highlighter
-      unstable.joplin-desktop
-      unstable.neovim
-      vscodium
+      joplin-desktop
+      unstable.vscodium
       xournalpp
-      emacs
     ];
   };
 }
